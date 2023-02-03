139f3921edd2e41eba38ba710a2b0f1cd135a2af
nagababu <nag4c1@gmail.com>
2023-02-03 10:40:12 +0530

    updating ./roboshop-jenkins

--- create-dns-records.sh
+++ create-dns-records.sh
@@ -10,4 +10,8 @@ echo '{
             "TTL": 15,
             "ResourceRecords": [{ "Value": "IPADDRESS"}]
           }}]
-      }' | sed -e "s/IPADDRESS/${IP}/"
+      }' | sed -e "s/IPADDRESS/${IP}/" >/tmp/jenkins.json
+
+
+ZONE_ID="Z0684247D5O5IEDBEU86"
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/jenkins.json |jq .
